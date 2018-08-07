0aad0b738d4efb24bb8325ad337733f011f3b131
nkundu <namrata.kundu96@hmail.com>
2018-08-07 11:31:46 +0530

    Printing addition, subtraction and multiplication

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,7 +8,22 @@ public class Main {
         String inputString2= new String("3x^2 + 2x + 4x^4 -3 + 6x^5");
         Polynomial p1=new Polynomial(inputString);
         Polynomial p2=new Polynomial(inputString2);
-        Polynomial result= p1.addPoly(p2);
-        result.printPoly();
+
+        System.out.println("Polynomial 1:"+ inputString);
+        System.out.println("Polynomial 1:"+ inputString2);
+
+        System.out.println();
+        
+        Polynomial result1= p1.addPoly(p2);
+        System.out.print("Addition: ");
+        result1.printPoly();
+
+        Polynomial result2= p1.subPoly(p2);
+        System.out.print("Subtraction: ");
+        result2.printPoly();
+
+        Polynomial result3= p1.addPoly(p2);
+        System.out.print("Multiplication: ");
+        result3.printPoly();
     }
 }
