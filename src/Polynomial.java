afdbc49bb09485624a7dab1e41e8de7e8af01c79
nkundu <namrata.kundu96@hmail.com>
2018-08-07 09:54:08 +0530

    Functions add and sub added

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -19,11 +19,52 @@ public class Polynomial {
 
 
     public Map<Integer,Integer> addPoly(Polynomial p1){
-        return new HashMap<>();
+
+        Map<Integer,Integer> result= new HashMap<>();
+
+        for(Map.Entry<Integer,Integer> entry : p1.mapPoly.entrySet()){
+            if(this.mapPoly.get(entry.getKey()) != null)
+            {
+                int z= this.mapPoly.get(entry.getKey());
+                int y= entry.getValue();
+                result.put(entry.getKey(), z+y);
+            }
+            else
+            {
+                result.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> entry : this.mapPoly.entrySet()){
+            if(p1.mapPoly.get(entry.getKey())==null){
+                result.put(entry.getKey(),entry.getValue());
+            }
+        }
+        return result;
     }
 
     public Map<Integer,Integer> subPoly(Polynomial p1){
-        return new HashMap<>();
+        Map<Integer,Integer> result= new HashMap<>();
+
+        for(Map.Entry<Integer,Integer> entry : p1.mapPoly.entrySet()){
+            if(this.mapPoly.get(entry.getKey()) != null)
+            {
+                int x= this.mapPoly.get(entry.getKey());
+                int y= entry.getValue();
+                result.put(entry.getKey(), x-y);
+            }
+            else{
+                result.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> entry : this.mapPoly.entrySet()){
+            if(p1.mapPoly.get(entry.getKey())==null){
+                result.put(entry.getKey(), entry.getValue() * (-1));
+            }
+        }
+
+        return result;
     }
 
     public Map<Integer,Integer> multPoly(Polynomial p1){
