d420f1e47db452b64aff2108dad64238d167adc2
vaagul <vaagul@gmail.com>
2018-08-07 08:54:04 +0530

    initial commit

---
+++ src/Polynomial.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Polynomial {
+    public static  String rawPoly = new String();
+    public Map<Integer,Integer> mapPoly= new HashMap<>();
+
+    public Polynomial(String rawPoly) {
+        this.rawPoly = rawPoly;
+    }
+
+    public Map<Integer,Integer> convert2map(String rawPoly){
+        return new HashMap<>();
+    }
+
+    public String convert2string(Map<Integer,Integer> mapPoly){
+        return new String();
+    }
+
+
+    public Map<Integer,Integer> addPoly(Polynomial p1){
+        return new HashMap<>();
+    }
+
+    public Map<Integer,Integer> subPoly(Polynomial p1){
+        return new HashMap<>();
+    }
+
+    public Map<Integer,Integer> multPoly(Polynomial p1){
+        return new HashMap<>();
+    }
+
+    public Map<Integer,Integer> divPoly(Polynomial p1){
+        return new HashMap<>();
+    }
+}
